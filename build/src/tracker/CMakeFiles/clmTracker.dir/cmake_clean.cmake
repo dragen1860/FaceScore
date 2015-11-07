1524e176666566ef03eed44606dad2c60e06141f
dragen1860 <dragen@live.cn>
2015-11-07 23:09:48 +0800

    modify face-fit related to score

---
+++ build/src/tracker/CMakeFiles/clmTracker.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clmTracker.dir/ATM.cpp.o"
+  "CMakeFiles/clmTracker.dir/FCheck.cpp.o"
+  "CMakeFiles/clmTracker.dir/IO.cpp.o"
+  "CMakeFiles/clmTracker.dir/Patch.cpp.o"
+  "CMakeFiles/clmTracker.dir/Detector.cpp.o"
+  "CMakeFiles/clmTracker.dir/ShapeModel.cpp.o"
+  "CMakeFiles/clmTracker.dir/Warp.cpp.o"
+  "CMakeFiles/clmTracker.dir/CLM.cpp.o"
+  "CMakeFiles/clmTracker.dir/FDet.cpp.o"
+  "CMakeFiles/clmTracker.dir/FaceTracker.cpp.o"
+  "CMakeFiles/clmTracker.dir/RegistrationCheck.cpp.o"
+  "CMakeFiles/clmTracker.dir/ShapePredictor.cpp.o"
+  "CMakeFiles/clmTracker.dir/myFaceTracker.cpp.o"
+  "../../lib/libclmTracker.pdb"
+  "../../lib/libclmTracker.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clmTracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/utils/CMakeFiles/utilities.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/utilities.dir/command-line-arguments.cpp.o"
+  "CMakeFiles/utilities.dir/helpers.cpp.o"
+  "CMakeFiles/utilities.dir/points.cpp.o"
+  "../../lib/libutilities.pdb"
+  "../../lib/libutilities.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/utilities.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
